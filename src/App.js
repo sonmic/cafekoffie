bd984e803203d9ec004ae5039a78a1ab546e834b
Sonmi Kos <sonmi.kos@gmail.com>
2019-09-04 23:52:55 -0500

    setup initial layout and theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import win01 from "./images/win01.png";
+import win02 from "./images/win02.png";
+import win03 from "./images/win03.png";
+import win04 from "./images/win04.png";
+import win05 from "./images/win05.png";
+import win06 from "./images/win06.png";
+import win07 from "./images/win07.png";
+import win08 from "./images/win08.png";
+import win09 from "./images/win09.png";
+import win10 from "./images/win10.png";
+import win11 from "./images/win11.png";
+import win12 from "./images/win12.png";
+import win13 from "./images/win13.png";
+import win14 from "./images/win14.png";
+import win15 from "./images/win15.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <header className="header">Header</header>
+      <div className="aside left">
+        <div className="title">CAFÉ KOFFIE</div>
+        <br />
+        <div className="subTitle">- CLICKY GAME -</div>
+        <br />
+        <div className="instruction01">
+          Click on an image to earn points, <br /> but don't click on any more
+          than once!
+        </div>
+
+        <br />
+
+        <div className="instruction02">Click an image to begin!</div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="currentScore">SCORE : </div>
+        <br />
+        <div className="topScore">TOP SCORE : </div>
+      </div>
+      <div className="aside right">
+        <div className="topFloor"></div>
+
+        <div className="windows"></div>
+        <div className="cafe">
+          <img className="cafe" src={require("./images/cafe.jpg")} />
+        </div>
+      </div>
+      <footer className="footer">
+        <div className="copyright">Footer</div>
+      </footer>
     </div>
   );
 }
